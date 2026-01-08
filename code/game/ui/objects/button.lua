0b18b7109572e2bf27d3d9e33abc3ef6e62a7f58
shiba <shibathedeveloper+github@gmail.com>
2026-01-08 20:20:48 +0100

    add button animation + new button sprites

--- code/game/ui/objects/button.lua
+++ code/game/ui/objects/button.lua
@@ -16,7 +16,10 @@ local Button = {
     hitboxElement = nil,
 
     mouseButton = 1,
-    onClick = nil
+    onClick = nil,
+
+    _clickScale = .1,
+    _scaleSpeed = .5
 }
 Button.__index = Button
 
@@ -24,10 +27,8 @@ local Module = {}
 Module._buttons = {}
 
 function Button:remove()
-    local id = self.id
-
-    Module._buttons[id] = nil
-    manager:release(id)
+    Module._buttons[self.id] = nil
+    manager:release(self.id)
 end
 
 function Button:click(x, y, mouseButton)
@@ -37,6 +38,14 @@ function Button:click(x, y, mouseButton)
 
     self.lastUsed = os.clock()
 
+    for _, e in pairs(self.elements) do
+        e._baseScaleX = e._baseScaleX or (e.scaleX or 1)
+        e._baseScaleY = e._baseScaleY or (e.scaleY or 1)
+
+        e.scaleX = e.scaleX - self._clickScale
+        e.scaleY = e.scaleY - self._clickScale
+    end
+
     if self.onClick then
         self:onClick()
     end
@@ -44,11 +53,22 @@ end
 
 function Button:update(deltaTime)
     if not self.hitboxElement then self:remove() return end
+
+    for _, element in pairs(self.elements) do
+        element._baseScaleX = element._baseScaleX or (element.scaleX or 1)
+        element._baseScaleY = element._baseScaleY or (element.scaleY or 1)
+
+        if element.scaleX < element._baseScaleX then
+            element.scaleX = math.min(element.scaleX + self._scaleSpeed * deltaTime, element._baseScaleX)
+        end
+        if element.scaleY < element._baseScaleY then
+            element.scaleY = math.min(element.scaleY + self._scaleSpeed * deltaTime, element._baseScaleY)
+        end
+    end
 end
 
 function Module:createButton(data)
     if not data then return end
-
     if not data.hitboxElement then return end
     if #data.elements == 0 then return end
 
@@ -57,17 +77,19 @@ function Module:createButton(data)
 
         elements = data.elements or {},
 
-        cooldown = data.cooldown or .25,
+        cooldown = data.cooldown or 0.25,
         lastUsed = 0,
 
         hitboxElement = data.hitboxElement,
 
         mouseButton = data.mouseButton or 1,
-        onClick = data.onClick or nil
+        onClick = data.onClick or nil,
+
+        _clickScale = data.clickScale or .1,
+        _scaleSpeed = data.scaleSpeed or .5
     }, Button)
 
     Module._buttons[button.id] = button
-
     return button
 end
 
